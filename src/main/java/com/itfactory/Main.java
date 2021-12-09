04ed2d9bbbb0516bfa8ef3c28231dc9b7a52270c
Dragos Roban <dragos.roban.training@gmail.com>
2021-12-09 18:53:36 +0200

    First commit

---
+++ src/main/java/com/itfactory/Main.java
@@ -0,0 +1,11 @@
+package com.itfactory;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduceti numele dumneavoastra: ");
+        System.out.println("Numele dumneavoastra este: " + scanner.nextLine());
+    }
+}
